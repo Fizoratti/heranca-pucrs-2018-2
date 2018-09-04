8f9bf05cee1ba8e90e223eed28412feec5624241
fizoratti <robson.felipe@acad.pucrs.br>
2018-09-04 13:16:12 -0300

    Add abstract classes

---
+++ src/Box.java
@@ -0,0 +1,13 @@
+package src;
+
+public abstract class Box {
+    private Object content;
+
+    public Box(Object content) {
+        this.content = content;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+}
---
+++ src/Package.java
@@ -0,0 +1,10 @@
+package src;
+
+public abstract class Package extends Box {
+
+    public Package(Object content) {
+        super(content);
+    }
+
+    public abstract double getVolume();
+}
